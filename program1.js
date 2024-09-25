51c8124a87e41badb511ca2b5a48b25e8e33a395
Abdul Rahim <abdulrahim43223@gmail.com>
2024-09-25 16:08:06 +0530

    solve problem1 and problem2

--- program1.js
+++ program1.js
@@ -2,10 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
-};
+var isValid=(s)=> {
+    const stack = [];
+    const parenthesesMap = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let char of s) {
+        if (parenthesesMap[char]) {
+            stack.push(char);
+        } else {
+            if (stack.length === 0 || parenthesesMap[stack.pop()] !== char) {
+                return false; 
+            }
+        }
+    }
+    return stack.length === 0;
+}
 
 module.exports = { isValid };
+
 
 
